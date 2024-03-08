ed8d9e42aac054f5b799b6f8991321ddec4bfc72
henrique <hrfacin@gmail.com>
2024-03-08 15:35:53 -0300

    configurando bindings e buffers, no desenvolvimento da pipeline finalmente deu o erro do tamanho xyz

--- test.py
+++ test.py
@@ -2,8 +2,8 @@ import numpy as np
 import wgpu
 import wgpu.backends.wgpu_native
 
-data = np.array([1])
-
+data = np.array([1,1,1,1])
+t_out = np.zeros(shape=data.shape, dtype= np.int32)
 device = wgpu.utils.get_default_device()
 
 cshader = None
@@ -15,3 +15,150 @@ with open('test_max.wgsl') as shader_file:
 
 max_bf = device.create_buffer(size=268435456, usage=wgpu.BufferUsage.STORAGE |
                                                         wgpu.BufferUsage.COPY_SRC)
+
+bin0 = device.create_buffer_with_data(data=data,
+                                            usage=wgpu.BufferUsage.STORAGE |
+                                                  wgpu.BufferUsage.COPY_DST |
+                                                  wgpu.BufferUsage.COPY_SRC)
+
+bif1 = device.create_buffer(size = 256,
+                                            usage=wgpu.BufferUsage.STORAGE |
+                                                  wgpu.BufferUsage.COPY_DST |
+                                                  wgpu.BufferUsage.COPY_SRC)
+
+bif2 = device.create_buffer_with_data(data=data,
+                                            usage=wgpu.BufferUsage.STORAGE |
+                                                  wgpu.BufferUsage.COPY_DST |
+                                                  wgpu.BufferUsage.COPY_SRC)
+
+bif3= device.create_buffer_with_data(data=data,
+                                            usage=wgpu.BufferUsage.STORAGE |
+                                                  wgpu.BufferUsage.COPY_DST |
+                                                  wgpu.BufferUsage.COPY_SRC)
+
+bif4 = device.create_buffer_with_data(data=data,
+                                            usage=wgpu.BufferUsage.STORAGE |
+                                                  wgpu.BufferUsage.COPY_DST |
+                                                  wgpu.BufferUsage.COPY_SRC)
+
+bif5 = device.create_buffer_with_data(data=data,
+                                            usage=wgpu.BufferUsage.STORAGE |
+                                                  wgpu.BufferUsage.COPY_DST |
+                                                  wgpu.BufferUsage.COPY_SRC)
+
+bif6 = device.create_buffer_with_data(data=data,
+                                            usage=wgpu.BufferUsage.STORAGE |
+                                                  wgpu.BufferUsage.COPY_DST |
+                                                  wgpu.BufferUsage.COPY_SRC)
+
+bif7 = device.create_buffer_with_data(data=data,
+                                            usage=wgpu.BufferUsage.STORAGE |
+                                                  wgpu.BufferUsage.COPY_DST |
+                                                  wgpu.BufferUsage.COPY_SRC)
+
+binding_layout = [
+    {
+        "binding": 0,
+        "visibility": wgpu.ShaderStage.COMPUTE,
+        "buffer": {
+            "type": wgpu.BufferBindingType.storage,
+        },
+    },
+    {
+        "binding": 1,
+        "visibility": wgpu.ShaderStage.COMPUTE,
+        "buffer": {
+            "type": wgpu.BufferBindingType.storage,
+        },
+    },
+    {
+        "binding": 2,
+        "visibility": wgpu.ShaderStage.COMPUTE,
+        "buffer": {
+            "type": wgpu.BufferBindingType.storage,
+        },
+    },
+    {
+        "binding": 3,
+        "visibility": wgpu.ShaderStage.COMPUTE,
+        "buffer": {
+            "type": wgpu.BufferBindingType.storage,
+        },
+    },
+{
+        "binding": 4,
+        "visibility": wgpu.ShaderStage.COMPUTE,
+        "buffer": {
+            "type": wgpu.BufferBindingType.storage,
+        },
+    },
+    {
+        "binding": 5,
+        "visibility": wgpu.ShaderStage.COMPUTE,
+        "buffer": {
+            "type": wgpu.BufferBindingType.storage,
+        },
+    },
+    {
+        "binding": 6,
+        "visibility": wgpu.ShaderStage.COMPUTE,
+        "buffer": {
+            "type": wgpu.BufferBindingType.storage,
+        },
+    },
+    {
+        "binding": 7,
+        "visibility": wgpu.ShaderStage.COMPUTE,
+        "buffer": {
+            "type": wgpu.BufferBindingType.storage,
+        },
+    },
+]
+
+#O BINDING 0 ESTÁ PEGANDO O MAX_BF INTEIRO???
+#COMO FAZ PARA LIMITAR QUANTO "OFFSET" UM BINDING PEGA?
+
+binding = [
+    {
+        "binding": 0,
+        "resource": {"buffer": max_bf, "offset": 0, "size": 1},
+    },
+    {
+        "binding": 1,
+        "resource": {"buffer": bif1, "offset": 0, "size": 1},
+    },
+    {
+        "binding": 2,
+        "resource": {"buffer": bif1, "offset": 32, "size": 1},
+    },
+    {
+        "binding": 3,
+        "resource": {"buffer": max_bf, "offset": 32, "size": 1},
+    },
+    {
+        "binding": 4,
+        "resource": {"buffer": max_bf, "offset": 64, "size": 1},
+    },
+    {
+        "binding": 5,
+        "resource": {"buffer": max_bf, "offset": 96, "size": 1},
+    },
+    {
+        "binding": 6,
+        "resource": {"buffer": max_bf, "offset": 128, "size": 1},
+    },
+    {
+        "binding": 7,
+        "resource": {"buffer": max_bf, "offset": 160, "size": 1},
+    },
+
+]
+
+bgl = device.create_bind_group_layout(entries=binding_layout)
+pipel = device.create_pipeline_layout(bind_group_layouts=[bgl])
+bgf = device.create_bind_group(layout=bgl, entries=binding)
+
+compute_big_t = device.create_compute_pipeline(
+    layout = pipel,
+    compute = {"module": cshader, "entry_point": "big_t"},
+)
--- test_max.wgsl
+++ test_max.wgsl
@@ -21,7 +21,7 @@ var<storage,read_write> data5 : array<i32>;
 var<storage,read_write> data6 : array<i32>;
 
 @group(7) @binding(0)
-    var<storage,read_write> data7 : array<i32>;
+var<storage,read_write> data7 : array<i32>;
 
 
 //var i: i32 = 0;
@@ -32,10 +32,22 @@ var<storage,read_write> data6 : array<i32>;
 //i++
 //}
 
+// NAO ESQUECER DE CONITNUAR DAQUI
+//  In wgpuDeviceCreateComputePipeline
+   //    Error matching shader requirements against the pipeline
+   //    Shader entry point's workgroup size [1024, 1024, 64] (67108864 total invocations) must be less or equal to the per-dimension limit [1024, 1024, 64] and the total invocation limit 1024
+
 @compute
-@workgroup_size(16384,16384,16384) // o MAX é 16384
-fn incr_k() {
+@workgroup_size(1024, 1024, 64) // o MAX é 16384
+fn big_t() {
     data0[0] = 1;
+    data0[1] = 2;
+    data0[2] = 3;
+    data0[3] = 4;
+    data0[4] = 5;
+    data0[5] = 6;
+    data0[6] = 7;
+    data0[7] = 8;
 }
 
 // Caso seja adicionado um oitavo grupo, será impossível computar esse arquivo
