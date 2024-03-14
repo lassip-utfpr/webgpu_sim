a0b3c1380908146bcc470b01c467d6fbb6caea9b
henrique <hrfacin@gmail.com>
2024-03-14 09:34:37 -0300

    em processo

--- test.py
+++ test.py
@@ -1,192 +1,81 @@
 import numpy as np
 import wgpu
-import wgpu.backends.wgpu_native
+
 
 data = np.array([1,1,1,1])
 datatest= np.array([1])
 t_out = np.zeros(shape=data.shape, dtype= np.int32)
 device = wgpu.utils.get_default_device()
 
-cshader = None
 with open('test_max.wgsl') as shader_file:
     cshader_str = shader_file.read()
     cshader = device.create_shader_module(code=cshader_str)
 
-#TAMANHO MAXIMO DO BUFFER
+bf00 = device.create_buffer_with_data(data=data, usage=wgpu.BufferUsage.STORAGE |
+                                                       wgpu.BufferUsage.COPY_DST |
+                                                       wgpu.BufferUsage.COPY_SRC)
 
-max_bf = device.create_buffer(size=268435456, usage=wgpu.BufferUsage.STORAGE |
-                                                    wgpu.BufferUsage.COPY_DST |
-                                                    wgpu.BufferUsage.COPY_SRC)
+bf01 = device.create_buffer_with_data(data= datatest, usage= wgpu.BufferUsage.STORAGE |
+                                                            wgpu.BufferUsage.COPY_DST |
+                                                            wgpu.BufferUsage.COPY_SRC)
 
-bin0 = device.create_buffer_with_data(data=data,
-                                            usage=wgpu.BufferUsage.STORAGE |
-                                                  wgpu.BufferUsage.COPY_DST |
-                                                  wgpu.BufferUsage.COPY_SRC)
-
-bif1 = device.create_buffer(size = 256,
-                                            usage=wgpu.BufferUsage.STORAGE |
-                                                  wgpu.BufferUsage.COPY_DST |
-                                                  wgpu.BufferUsage.COPY_SRC)
-
-bif2 = device.create_buffer_with_data(data=data,
-                                            usage=wgpu.BufferUsage.STORAGE |
-                                                  wgpu.BufferUsage.COPY_DST |
-                                                  wgpu.BufferUsage.COPY_SRC)
-
-bif3= device.create_buffer_with_data(data=data,
-                                            usage=wgpu.BufferUsage.STORAGE |
-                                                  wgpu.BufferUsage.COPY_DST |
-                                                  wgpu.BufferUsage.COPY_SRC)
-
-bif4 = device.create_buffer_with_data(data=data,
-                                            usage=wgpu.BufferUsage.STORAGE |
-                                                  wgpu.BufferUsage.COPY_DST |
-                                                  wgpu.BufferUsage.COPY_SRC)
-
-bif5 = device.create_buffer_with_data(data=data,
-                                            usage=wgpu.BufferUsage.STORAGE |
-                                                  wgpu.BufferUsage.COPY_DST |
-                                                  wgpu.BufferUsage.COPY_SRC)
-
-bif6 = device.create_buffer_with_data(data=data,
-                                            usage=wgpu.BufferUsage.STORAGE |
-                                                  wgpu.BufferUsage.COPY_DST |
-                                                  wgpu.BufferUsage.COPY_SRC)
-
-bif7 = device.create_buffer_with_data(data=datatest,
-                                            usage=wgpu.BufferUsage.STORAGE |
-                                                  wgpu.BufferUsage.COPY_DST |
-                                                  wgpu.BufferUsage.COPY_SRC)
-
-binding_layout = [
+bg_layout = [
     {
         "binding": 0,
         "visibility": wgpu.ShaderStage.COMPUTE,
-        "buffer": {
+        "buffer":{
             "type": wgpu.BufferBindingType.storage,
         },
     },
+]
+
+bg1_layout = [
     {
         "binding": 1,
         "visibility": wgpu.ShaderStage.COMPUTE,
-        "buffer": {
-            "type": wgpu.BufferBindingType.storage,
-        },
-    },
-    {
-        "binding": 2,
-        "visibility": wgpu.ShaderStage.COMPUTE,
-        "buffer": {
-            "type": wgpu.BufferBindingType.storage,
-        },
-    },
-    {
-        "binding": 3,
-        "visibility": wgpu.ShaderStage.COMPUTE,
-        "buffer": {
-            "type": wgpu.BufferBindingType.storage,
-        },
-    },
-    {
-        "binding": 4,
-        "visibility": wgpu.ShaderStage.COMPUTE,
-        "buffer": {
-            "type": wgpu.BufferBindingType.storage,
-        },
-    },
-    {
-        "binding": 5,
-        "visibility": wgpu.ShaderStage.COMPUTE,
-        "buffer": {
-            "type": wgpu.BufferBindingType.storage,
-        },
-    },
-    {
-        "binding": 6,
-        "visibility": wgpu.ShaderStage.COMPUTE,
-        "buffer": {
-            "type": wgpu.BufferBindingType.storage,
-        },
-    },
-
-]
-
-binding_layout7 = [
-    {
-        "binding": 7,
-        "visibility": wgpu.ShaderStage.COMPUTE,
-        "buffer": {
+        "buffer":{
             "type": wgpu.BufferBindingType.storage,
         },
     },
 ]
 
-binding_group = [
+bg1_config = [
+    {
+        "binding": 1,
+        "resource":{"buffer": bf01, "offset":0, "size":bf01.size},
+    },
+
+]
+
+bg_config = [
     {
         "binding": 0,
-        "resource": {"buffer": max_bf, "offset": 0, "size": max_bf.size},
+        "resource":{"buffer":bf00, "offset":0, "size":bf00.size},
     },
-    {
-        "binding": 1,
-        "resource": {"buffer": bif1, "offset": 0, "size": bif1.size},
-    },
-    {
-        "binding": 2,
-        "resource": {"buffer": bif2, "offset": 0, "size": bif2.size},
-    },
-    {
-        "binding": 3,
-        "resource": {"buffer": bif3, "offset": 0, "size": bif3.size},
-    },
-    {
-        "binding": 4,
-        "resource": {"buffer": bif4, "offset": 0, "size": bif4.size},
-    },
-    {
-        "binding": 5,
-        "resource": {"buffer": bif5, "offset": 0, "size": bif5.size},
-    },
-    {
-        "binding": 6,
-        "resource": {"buffer": bif6, "offset": 0, "size": bif6.size},
-    },
-
 ]
 
-binding_group7 = [
-    {
-        "binding": 7,
-        "resource": {"buffer": bif7, "offset": 0, "size": bif7.size},
-    },
+bgl= device.create_bind_group_layout(entries=bg_layout)
+bgl1= device.create_bind_group_layout(entries=bg1_layout)
 
-]
+pipeline= device.create_pipeline_layout(bind_group_layouts=[bgl,bgl1])
 
-bgl = device.create_bind_group_layout(entries=binding_layout)
-bgl7 = device.create_bind_group_layout(entries=binding_layout7)
-pipeline = device.create_pipeline_layout(bind_group_layouts=[bgl,bgl7])
-bgf = device.create_bind_group(layout=bgl, entries=binding_group)
-bgf7 = device.create_bind_group(layout=bgl7, entries=binding_group7)
-
-
-
-# compute_big_t = device.create_compute_pipeline(
-#     layout= pipeline,
-#     compute= {"module":cshader, "entry_point":"big_t"},
-# )
+bgf= device.create_bind_group(layout=bgl, entries=bg_config)
+bgf1= device.create_bind_group(layout= bgl1, entries= bg1_config)
 
 compute_incr_h = device.create_compute_pipeline(
-    layout= pipeline,
-    compute= {"module":cshader, "entry_point":"incr_h"},
+    layout = pipeline,
+    compute = {"module": cshader, "entry_point": "incr_h"},
 )
 
+command_encoder = device.create_command_encoder()
+compute_pass = command_encoder.begin_compute_pass()
 
+compute_pass.set_bind_group(0,bgf,[],0,999999)
+compute_pass.set_bind_group(1,bgf1,[],0,999999)
 
-
-
-
-
-
-
-
-
-
+compute_pass.set_pipeline(compute_incr_h)
+compute_pass.dispatch_workgroups(1)
+compute_pass.end()
+device.queue.submit([command_encoder.finish()])
+t_out= device.queue.read_buffer(bf00).cast("i").tolist()
+print(t_out)
--- test_max.wgsl
+++ test_max.wgsl
@@ -5,75 +5,10 @@ var<storage,read_write> data0 : array<i32>;
 @group(1) @binding(1)
 var<storage,read_write> data1 : array<i32>;
 
-@group(2) @binding(2)
-var<storage,read_write> data2 : array<i32>;
-
-@group(3) @binding(3)
-var<storage,read_write> data3 : array<i32>;
-
-@group(4) @binding(4)
-var<storage,read_write> data4 : array<i32>;
-
-@group(5) @binding(5)
-var<storage,read_write> data5 : array<i32>;
-
-@group(6) @binding(6)
-var<storage,read_write> data6 : array<i32>;
-
-@group(7) @binding(7)
-var<storage,read_write> data7 : array<i32>;
-
 @compute
-@workgroup_size(16,1,64)
+@workgroup_size(1)
 fn incr_h(){
 
-data7[0]=1;
-}
-
-@compute
-@workgroup_size(16384) // o MAX é 16384 quando programado em wgsl, entretanto quando utilizado como shader no py seu limite é 1024,1024,64 MAS o numero total no workgroup tem que ser menor que 1024
-fn big_t() {
-//    data3[0]=1;
-//    data3[3] = 4;
-//    data3[4] = 5;
-//    data3[5] = 6;
-//    data3[6] = 7;
-//    data3[999999999] = 8;
-//    data4[0] = 1;
-//    data4[1] = 2;
-//    data4[2] = 3;
-//    data4[3] = 4;
-//    data4[4] = 5;
-//    data4[5] = 6;
-//    data4[6] = 7;
-//    data4[999999999] = 8;
-//    data5[0] = 1;
-//    data5[1] = 2;
-//    data5[2] = 3;
-//    data5[3] = 4;
-//    data5[4] = 5;
-//    data5[5] = 6;
-//    data5[6] = 7;
-//    data5[999999999] = 8;
-//    data6[0] = 1;
-//    data6[1] = 2;
-//    data6[2] = 3;
-//    data6[3] = 4;
-//    data6[4] = 5;
-//    data6[5] = 6;
-//    data6[6] = 7;
-//    data6[999999999] = 8;
-    data7[0] = 1;
-//    data7[1] = 2;
-//    data7[2] = 3;
-//    data7[3] = 4;
-//    data7[4] = 5;
-//    data7[5] = 6;
-//    data7[6] = 7;
-//    data7[999999999] = 8;
+data1[0]=1;
 
 }
-
-// Caso seja adicionado um oitavo grupo, será impossível computar esse arquivo
-// @group(8) @binding(0)
-//var<storage,read_write> data8 : array<i32>;
