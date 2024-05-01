107c9873be13bbf6173068cc5874d674381e1aa8
giovanni <giovanni@utfpr.edu.br>
2024-05-01 12:12:35 -0300

    Inclusão de novas funções de acesso aos atributos e também de função que calcula os coeficientes para cálculo da CPML.

--- simul_utils.py
+++ simul_utils.py
@@ -162,6 +162,19 @@ class SimulationROI:
         # Quantidade adicional de pontos em cada lado da ROI.
         self._pad = pad
 
+        # Vetores com os valores dos coeficientes para cálculo da PML
+        if self._pml_xmin_len + self._pml_xmax_len > 0:
+            self._pml_full_x = np.zeros((self._pml_xmin_len + self._pml_xmax_len + self._w_len, 3))
+            self._pml_half_x = np.zeros((self._pml_xmin_len + self._pml_xmax_len + self._w_len, 3))
+
+        if self._pml_ymin_len + self._pml_ymax_len > 0:
+            self._pml_full_y = np.zeros((self._pml_ymin_len + self._pml_ymax_len + self._d_len, 3))
+            self._pml_half_y = np.zeros((self._pml_ymin_len + self._pml_ymax_len + self._d_len, 3))
+
+        if self._pml_zmin_len + self._pml_zmax_len > 0:
+            self._pml_full_z = np.zeros((self._pml_zmin_len + self._pml_zmax_len + self._h_len, 3))
+            self._pml_half_z = np.zeros((self._pml_zmin_len + self._pml_zmax_len + self._h_len, 3))
+
     def get_nx(self):
         return self._w_len + self._pml_xmin_len + self._pml_xmax_len + 2 * self._pad
 
@@ -198,6 +211,15 @@ class SimulationROI:
     def get_iz_max(self):
         return self._h_len + self._pml_zmin_len + self._pad
 
+    def get_pml_thickness_x(self):
+        return (self._pml_xmin_len + self._pml_xmax_len) * self.w_step
+
+    def get_pml_thickness_y(self):
+        return (self._pml_ymin_len + self._pml_ymax_len) * self.d_step
+
+    def get_pml_thickness_z(self):
+        return (self._pml_zmin_len + self._pml_zmax_len) * self.h_step
+
     def get_nearest_grid_idx(self, point):
         """Método para retornar os índices mais próximos da grade para o ponto da ROI fornecido."""
         if type(point) is not np.ndarray and type(point) is list:
@@ -216,6 +238,64 @@ class SimulationROI:
         iy = np.absolute(self.d_points - point[1]).argmin() + self._pml_ymin_len + self._pad
         iz = np.absolute(self.h_points - point[2]).argmin() + self._pml_zmin_len + self._pad
         return [ix, iy, iz]
+
+    def calc_pml_array(self, axis='x', grid='f', dt=1.0, d0=1.0, npower=2.0, alpha_max=30.0, k_max=1.0):
+        """Metodo que calcula os vetores com os valores para implementar a camada de PML"""
+        # Origem da PML (posicao das bordas direita e esquerda menos a espessura, em unidades de distancia)
+        if axis == 'x' or axis == 'X':
+            delta = self.w_step
+            tam_pml = self._w_len + self._pml_xmin_len + self._pml_xmax_len
+            orig_left = self._pml_xmin_len * delta
+            orig_right = (self._pml_xmin_len + self._w_len - 1) * delta
+            thickness_pml_left = self._pml_xmin_len * delta
+            thickness_pml_right = self._pml_xmax_len * delta
+        elif axis == 'y' or axis == 'Y':
+            delta = self.d_step
+            tam_pml = self._d_len + self._pml_ymin_len + self._pml_ymax_len
+            orig_left = self._pml_ymin_len * delta
+            orig_right = (self._pml_ymin_len + self._d_len - 1) * delta
+            thickness_pml_left = self._pml_ymin_len * delta
+            thickness_pml_right = self._pml_ymax_len * delta
+        elif axis == 'z' or axis == 'Z':
+            delta = self.h_step
+            tam_pml = self._h_len + self._pml_zmin_len + self._pml_zmax_len
+            orig_left = self._pml_zmin_len * delta
+            orig_right = (self._pml_zmin_len + self._h_len - 1) * delta
+            thickness_pml_left = self._pml_zmin_len * delta
+            thickness_pml_right = self._pml_zmax_len * delta
+        else:
+            raise IndexError(f"'axis' = {axis} not supported")
+
+        # Incicializacao para full ou half grid
+        val = delta * np.arange(tam_pml)
+        if grid == 'f' or grid == 'F':
+            val_pml_left = orig_left - val
+            val_pml_right = val - orig_right
+        elif grid == 'h' or grid == 'H':
+            val_pml_left = orig_left - (val + delta / 2.0)
+            val_pml_right = (val + delta / 2.0) - orig_right
+        else:
+            raise IndexError(f"'grid' = {grid} not supported")
+
+        # Calculo dos coeficientes
+        pml_mask_left = np.where(val_pml_left < 0.0, False, True)
+        pml_mask_right = np.where(val_pml_right < 0.0, False, True)
+        mask = np.logical_or(pml_mask_left, pml_mask_right)
+        pml = np.zeros(tam_pml)
+        pml[pml_mask_left] = val_pml_left[pml_mask_left] / thickness_pml_left
+        pml[pml_mask_right] = val_pml_right[pml_mask_right] / thickness_pml_right
+        d = (d0 * pml ** npower).astype(np.float32)
+        k = (1.0 + (k_max - 1.0) * pml ** npower).astype(np.float32)
+        alpha = (alpha_max * (1.0 - np.where(mask, pml, 1.0))).astype(np.float32)
+        b = np.exp(-(d / k + alpha) * dt).astype(np.float32)
+        a = np.zeros(tam_pml, dtype=np.float32)
+        i = np.where(d > 1e-6)
+        a[i] = d[i] * (b[i] - 1.0) / (k[i] * (d[i] + k[i] * alpha[i]))
+
+        return a, b, k
+
+    # def get_pml_arrays(self):
+    #     """Metodo que calcula e retorna os vetores como os valores para implementar a camada de PML"""
 
     # def get_coord(self):
     #     """Método que retorna todas as coordenadas da ROI (*mesh*) no formato
